06e3dfc10360377a161b249de815be91646eaed0
Thilina Abhisheka <58887211+t-abhisheka@users.noreply.github.com>
2023-08-13 14:51:01 +0530

    final

--- sketch_aug8a.ino
+++ sketch_aug8a.ino
@@ -1,36 +1,37 @@
 //Initializing Blynk app
-#define BLYNK_TEMPLATE_ID           "TMPL69FYO9klc"                         //Blynk templete id
-#define BLYNK_TEMPLATE_NAME         "new project"                           //Blynk template name
-#define BLYNK_AUTH_TOKEN            "x1opGYC14M9To1j4HwuPKCHWvUQEsDae"      //Blynk auth code
+#define BLYNK_TEMPLATE_ID           ""
+#define BLYNK_TEMPLATE_NAME         ""
+#define BLYNK_AUTH_TOKEN            ""
 
 // Importing nesessory libraries
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#include <Servo.h>
-#include <Time.h>
-#include <TimeAlarms.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
+#include <ESP8266WiFi.h>              // For Board Control
+#include <BlynkSimpleEsp8266.h>       // For Link Board With BLYNK App
+#include <Servo.h>                    // Control Servo Motor
+#include <Time.h>                     // For Time Keeping
+#include <TimeAlarms.h>               // For Alarms
+#include <NTPClient.h>                // For Time Keeping
+#include <WiFiUdp.h>                  // For Time Keeping
 
-// WiFi credentials
+// WiFi Credentials
 char ssid[] = "Routz";
 char pass[] = "12341234";
 
 // Initializing Servo Motor
-Servo servo1, servo2;
+Servo servo;
 int switch_state = 0;
 
 // Initializing Power Cut Handler
 int sensorValue = 0;
 int sensorPin = A0;
 
+// Initializing Serial
 #define BLYNK_PRINT Serial
 
-// Time functions
-
+// Connect to NTP Server
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "lk.pool.ntp.org");
 
+// Time Handler
 void time_handle(){
   timeClient.update();
   timeClient.setTimeOffset(19800);
@@ -38,13 +39,13 @@ void time_handle(){
   int MM = timeClient.getMinutes();
   int SS = timeClient.getSeconds();
   setTime(HH, MM, SS, 8, 10, 23);
-
 }
 
+// Getting time from BLYNK Server
 BLYNK_CONNECTED() {
   Blynk.sendInternal("rtc", "iso");
-  Serial.println("connected");
-  Serial.println("connected");
+  Serial.println("");
+  Serial.println("Connected to the Server");
 }
 
 // Control Servo  
@@ -52,32 +53,41 @@ BLYNK_WRITE(V0){
   switch_state = param.asInt();
   if(switch_state == 0){
     turn_off();
-  }
-  else{
+    Serial.println("Switch was Turned Off Through The App or Google Assistant");
+  }else{
     turn_on();
+    Serial.println("Switch was Turned On Through The App or Google Assistant");
   }
 }
 
+// Servor Turn Off Function
 void turn_off(){
-  servo1.write(180);
-  delay(500);
-  servo1.write(0);
-  Serial.println("off");
+  servo.write(0);
 }
 
+// Servor Turn On Function
 void turn_on(){
-  servo2.write(0);
-  delay(500);
-  servo2.write(180);
-  Serial.println("on");
+  servo.write(60);
 }
 
 // Alarm Functions
 void alarm(int hh, int mm, int ss, int status){
   if(status == 0){
     Alarm.alarmRepeat(hh,mm,ss,turn_off);
+    Serial.print("Switch will Off at ");
+    Serial.print(hh);
+    Serial.print(":");
+    Serial.print(mm);
+    Serial.print(":");
+    Serial.println(ss);
   }else if(status == 1){
     Alarm.alarmRepeat(hh,mm,ss,turn_on);
+    Serial.print("Switch will On at ");
+    Serial.print(hh);
+    Serial.print(":");
+    Serial.print(mm);
+    Serial.print(":");
+    Serial.println(ss);
   }
 }
 
@@ -93,23 +103,21 @@ void power_cut_handle(int default_value){
 
   if(sensorValue == 0 && set_to_deafult ==0){
     if(default_value == 0){
+      Serial.print("Sensor Value : ");
+      Serial.println(sensorValue);
       turn_off();
+      Serial.println("Switch was Turned Off by Power Cut Handler");
       set_to_deafult = 1;
-      Serial.println("set to default 0");
-
     }else{
+      Serial.print("Sensor Value : ");
+      Serial.println(sensorValue);
       turn_on();
+      Serial.println("Switch was Turned On by Power Cut Handler");
       set_to_deafult = 1;
-      Serial.println("set to default 1");
     }
   }else if(sensorValue == 1024){
     set_to_deafult = 0;
-    Serial.println("reset default");
   }
-  Serial.print("sensorValue ");
-  Serial.println(sensorValue);
-  Serial.print("set_to_deafult ");
-  Serial.println(set_to_deafult);
 }
 
 void setup(){
@@ -121,24 +129,17 @@ void setup(){
   
   // Set Alarms
   time_handle();
-  alarm(18,2,0, 1);
-  alarm(18,2,10, 0);
-  alarm(18,2,15, 1);
-  alarm(18,2,20, 0);
+  alarm(12,57,0, 1);
+  alarm(12,57,10, 0);
+  alarm(12,57,15, 1);
+  alarm(12,57,20, 0);
 
-  // Initial Servo1 Position
-  servo1.attach(2);
-  servo1.write(0); 
-
-  // Initial Servo2 Position
-  servo2.attach(0);
-  servo2.write(180); 
+  // Initial Servo
+  servo.attach(2,500,2500);
 }
 
 void loop(){
-  Blynk.run();
-  Alarm.delay(200);
-  power_cut_handle(0);
-  delay(1500);
-  Serial.println(analogRead(sensorPin));
+  Blynk.run();                              // Run BYLNK app
+  Alarm.delay(200);                         // Run Alarms
+  power_cut_handle(0);                      // Run Power Cut Handler
 }
